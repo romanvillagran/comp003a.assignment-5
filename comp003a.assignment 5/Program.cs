732c7d42414945d4c71342957493fd21e555eb35
Your Name <you@example.com>
2025-02-12 18:42:25 -0800

    Add project files.

---
+++ comp003a.assignment 5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace comp003a.assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
