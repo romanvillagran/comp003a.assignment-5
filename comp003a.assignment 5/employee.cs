8938fe3e26763cac8dccebfd1d9e3e80804b0036
Your Name <you@example.com>
2025-02-15 13:24:01 -0800

    added the constructor and fergot to make the employee id null so i have to add some stuff to fix that

--- comp003a.assignment 5/employee.cs
+++ comp003a.assignment 5/employee.cs
@@ -19,6 +19,14 @@ namespace comp003a.assignment_5
         public string EmployeeId
         {
             get { return _employeeId; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Cannot be empty or null");
+                }
+                _employeeId = value;
+            }
             
         }
 
@@ -67,6 +75,32 @@ namespace comp003a.assignment_5
                 }
                 _salary = value;
             }
+        }
+
+        //this the constructor
+        public employee(string employeeId, string firstName, string lastName, double salary, string middleName = "")
+        {
+            EmployeeId = employeeId;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            MiddleName = middleName;
+        }
+
+        //prints the names 
+        public void printFullName()
+        {
+            Console.WriteLine($"First name: {FirstName}");
+            Console.WriteLine($"Middle Name: {MiddleName}");
+            Console.WriteLine($"Last Name: {LastName}");
+        }
+
+        //this displays employee stuff like id and salary
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeId}");
+            printFullName();
+            Console.WriteLine($"Salary: {Salary}");
         }
 
 
