9404aab6e2750216a1ec5be1f662f0432cefadc7
Your Name <you@example.com>
2025-02-14 22:02:13 -0800

    added the first part of the employee i think dont know if im doing it right or its supposed to be done a different way

---
+++ comp003a.assignment 5/employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp003a.assignment_5
+{
+    internal class employee
+    {
+        private string _employeeId;
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        private double _salary;
+
+
+        public string EmployeeId
+        {
+            get { return _employeeId; }
+        }
+
+        public string FirstName
+            { get { return _firstName; } }
+
+        public string MiddleName
+            { get { return _middleName; } }
+
+        public string LastName
+            { get { return _lastName; } }
+
+        public double Salary
+            { get { return _salary; } }
+
+
+    }
+}
